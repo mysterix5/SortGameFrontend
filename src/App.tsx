b3287770435b6ea82270ba4a5f95648b29ba0ca2
Lukas <lukasduechting@mailbox.org>
2022-07-08 15:29:39 +0200

    use BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import './App.css';
 import SavedGamesOverview from "./components/Main";
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-        <SavedGamesOverview/>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<SavedGamesOverview/>}/>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    )
+        ;
 }
 
 export default App;
