ba4f72c69942a333c7facf9a65c14799fcab2753
Lukas <lukasduechting@mailbox.org>
2022-07-09 13:39:33 +0200

    disable eslint on useEffect deps

--- src/components/GameComponent.tsx
+++ src/components/GameComponent.tsx
@@ -24,6 +24,7 @@ export default function GameComponent() {
     useEffect(() => {
         console.log(id);
         fetchGameById(id!);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [id])
 
     function fetchGameById(id: string) {
